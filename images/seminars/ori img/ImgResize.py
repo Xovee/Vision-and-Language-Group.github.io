14a968e009eed18a00d396fef437b8243e81d5d7
Invokar <931841310@qq.com>
2019-11-18 13:28:01 +0800

    update sem

--- images/seminars/ori img/ImgResize.py
+++ images/seminars/ori img/ImgResize.py
@@ -1,10 +1,12 @@
 import os
 import cv2
 
+print('jj')
 
 def img_resize(root, shape):
 	for file in os.listdir(root):
 		if file.split('.')[-1] in ['jpg', 'png']:
+
 			img = cv2.imread(file)
 			img = cv2.resize(img, shape, interpolation=cv2.INTER_AREA)
 			cv2.imwrite('../' + file, img)
